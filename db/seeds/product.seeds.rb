d41e8d828603d1c209d1a886ad806f42b8af1038
Razwan Rashidi <razwan.izmi@gmail.com>
2017-09-08 13:47:02 +0800

    Change seedfile

--- db/seeds/product.seeds.rb
+++ db/seeds/product.seeds.rb
@@ -1,5 +1,32 @@
+Product.delete_all
+ActiveRecord::Base.connection.reset_pk_sequence!('products')
+
 Product.create!(
   name: 'Mince Gula-gula Kurus',
   price: 45.00,
-  image_url: Rails.root.join('public', 'images', 'mince.jpg').to_s
+  image_url: 'https://orderlink.herokuapp.com/images/mince.jpg'
+)
+
+Product.create!(
+  name: 'Necklace Trend Star',
+  price: 19.00,
+  image_url: 'https://orderlink.herokuapp.com/images/necklace.jpg'
+)
+
+Product.create!(
+  name: 'iPhone7 Premium Ultra Thin Case',
+  price: 109.00,
+  image_url: 'https://orderlink.herokuapp.com/images/case.jpg'
+)
+
+Product.create!(
+  name: 'Sanaa Instant Scarf',
+  price: 55.00,
+  image_url: 'https://orderlink.herokuapp.com/images/scarf.jpg'
+)
+
+Product.create!(
+  name: 'Car Steering Phone Holder',
+  price: 25.00,
+  image_url: 'https://orderlink.herokuapp.com/images/holder.jpg'
 )
