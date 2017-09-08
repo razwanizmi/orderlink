b48108a3c28720e7fe0b75239c6391822d8345c9
Razwan Rashidi <razwan.izmi@gmail.com>
2017-09-08 12:27:05 +0800

    Create product model

---
+++ db/migrate/20170908042608_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :details
+      t.float :price
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/products.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :product do
+    name "MyString"
+    details "MyString"
+    price 1.5
+    image_url "MyString"
+  end
+end
