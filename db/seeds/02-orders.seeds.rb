58c176afc6530da787f62fe06e1a6d572f68fedc
Razwan Rashidi <razwan.izmi@gmail.com>
2017-09-09 07:19:44 +0800

    Change seed file

--- db/seeds/02-orders.seeds.rb
+++ db/seeds/02-orders.seeds.rb
@@ -3,7 +3,7 @@
 Order.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!('orders')
 
-6.times do
+4.times do
   order = Order.create!(
     name: Faker::Name.name,
     address_one: Faker::Address.street_address,
@@ -23,3 +23,23 @@ ActiveRecord::Base.connection.reset_pk_sequence!('orders')
     )
   end
 end
+
+2.times do
+  order = Order.create!(
+    name: Faker::Name.name,
+    address_one: Faker::Address.street_address,
+    address_two: '',
+    city: ['Shah Alam', 'Petaling Jaya', 'Subang Jaya', 'Cyberjaya'].sample,
+    state: 'Selangor',
+    postcode: rand(10000..99999),
+    phone_number: "01#{rand(10000000..99999999)}",
+    paid: true,
+    delivered: true
+  )
+
+  Product.all.sample([1, 2, 3].sample).each do |product|
+    order.order_products.create(
+      product_id: product.id,
+      quantity: [1, 2].sample
+    )
+  end
