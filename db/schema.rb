4f590ca577dd0bb8a170e4e89e3d0c76460fc6f5
Razwan Rashidi <razwan.izmi@gmail.com>
2017-09-08 14:26:44 +0800

    Add order products

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908061148) do
+ActiveRecord::Schema.define(version: 20170908062504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "order_products", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "product_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string "link"
@@ -38,4 +48,6 @@ ActiveRecord::Schema.define(version: 20170908061148) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "order_products", "orders"
+  add_foreign_key "order_products", "products"
 end
